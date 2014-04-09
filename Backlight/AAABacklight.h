30d1c87a2e2cf68e96d0183c551383e24b04a4a9
Aliaksandr Andrashuk <limejelly@yandex.ru>
2014-04-09 19:07:46 +0300

    Initial Commit

---
+++ Backlight/AAABacklight.h
@@ -0,0 +1,13 @@
+//
+//  AAABacklight.h
+//  AAABacklight
+//
+//  Created by Aliksandr Andrashuk on 09.04.14.
+//  Copyright (c) 2014 Aliksandr Andrashuk. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface AAABacklight : NSObject
+
+@end
