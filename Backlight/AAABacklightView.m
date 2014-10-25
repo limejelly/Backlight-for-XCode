591a2fe8aa992f396e3f8cec8e47469ab7bb980a
Christoffer Winterkvist <chris@zenangst.com>
2014-10-25 23:10:02 +0200

    Refactor AAABacklightView

--- Backlight/AAABacklightView.m
+++ Backlight/AAABacklightView.m
@@ -13,18 +13,26 @@
 - (void)drawRect:(NSRect)dirtyRect
 {
     [super drawRect:dirtyRect];
-	
-	NSColor *color = self.backlightColor;
-	if (color == nil) {
 
-		color = [NSColor alternateSelectedControlColor];
-	}
+    CGFloat padding = 2.5f;
+    CGFloat radius = 4.0f;
+
+    NSRect rect = dirtyRect;
+    rect.size.width -= (padding * 2);
+    rect.origin.x += padding;
+
+    NSColor *color = (self.backlightColor) ?: [NSColor alternateSelectedControlColor];
     [[color colorWithAlphaComponent:0.2] setFill];
-    NSRectFillUsingOperation(dirtyRect, NSCompositeSourceOver);
+
+    [NSBezierPath setDefaultLineWidth:1.0];
+    NSBezierPath *path = [NSBezierPath bezierPathWithRoundedRect:rect
+                                                         xRadius:radius yRadius:radius];
+
+    [[color colorWithAlphaComponent:0.2] set];
+    [path fill];
 }
 
 - (void)setBacklightColor:(NSColor *)backlightColor {
-	
 	_backlightColor = backlightColor;
 	[self setNeedsDisplay:YES];
 }
